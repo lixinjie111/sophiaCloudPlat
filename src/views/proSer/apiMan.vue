be023ebfec4015d15ef3fc598a7a5348669d1d9b
zjj <660403@gdpr.com>
2021-01-13 11:20:00 +0800

    API模板修改

--- src/views/proSer/apiMan.vue
+++ src/views/proSer/apiMan.vue
@@ -28,14 +28,28 @@
     <div class="fangwen_area_container">
       <div class="title_container">访问趋势</div>
       <div class="bar_container" id="barID"></div>
+      <div class="fangwen_detail_container" v-show="ifShowDetail">
+        <a-table :columns="qscolumns" :data-source="qsdata">
+          <a slot="servName" slot-scope="text" href="javascript:;">{{text}}</a>
+          <span slot="usePer" slot-scope="usePer">
+            <a-progress :percent="usePer" size="small" status="active" />
+          </span>
+          <a slot="openBuy" slot-scope="text" href="javascript:;">{{text}}</a>
+          <a slot="buyNum" slot-scope="text" href="javascript:;">{{text}}</a>
+        </a-table>
+      </div>
       <div class="expan_btn_container">
-        <div class="btn_container1"></div>
+        <div class="btn_container1" @click="expanfangwen"></div>
       </div>
     </div>
     <div class="nengli_area_container">
       <div class="nengli_title_container">能力服务列表</div>
       <div class="table_container">
-        <a-table :columns="columns" :data-source="data"></a-table>
+        <a-table :columns="nlcolumns" :data-source="nldata">
+          <a slot="apiname" class="ant-dropdown-link" slot-scope="text">{{ text }}</a>
+          <a slot="openBuy" class="ant-dropdown-link" slot-scope="text">{{ text }}</a>
+          <a slot="Purchases" class="ant-dropdown-link" slot-scope="text">{{ text }}</a>
+        </a-table>
       </div>
     </div>
   </div>
@@ -48,11 +62,12 @@ export default {
   data() {
     return {
       locale,
-      columns: [
+      nlcolumns: [
         {
           title: "API名称",
           dataIndex: "apiname",
-          key: "apiname"
+          key: "apiname",
+          scopedSlots: { customRender: "apiname" }
         },
         {
           title: "消费状态",
@@ -82,15 +97,19 @@ export default {
         {
           title: "开通付费",
           key: "openBuy",
-          dataIndex: "openBuy"
+          dataIndex: "openBuy",
+          slots: { title: "customTitle" },
+          scopedSlots: { customRender: "apiname" }
         },
         {
           title: "购买次数包",
           key: "Purchases",
-          dataIndex: "Purchases"
+          dataIndex: "Purchases",
+          slots: { title: "customTitle" },
+          scopedSlots: { customRender: "apiname" }
         }
       ],
-      data: [
+      nldata: [
         {
           key: "1",
           apiname: "语音识别-普通话",
@@ -124,7 +143,81 @@ export default {
           openBuy: "购买",
           Purchases: "购买"
         }
-      ]
+      ],
+      qscolumns: [
+        {
+          title: "服务名称",
+          dataIndex: "servName",
+          key: "servName",
+          scopedSlots: { customRender: "servName" }
+        },
+        {
+          title: "访问量",
+          dataIndex: "fwNum",
+          key: "fwNum"
+        },
+        {
+          title: "剩余用量",
+          dataIndex: "syNum",
+          key: "syNum"
+        },
+        {
+          title: "同上月相比",
+          key: "tsyPer",
+          dataIndex: "tsyPer"
+        },
+        {
+          title: "使用率占比",
+          key: "usePer",
+          dataIndex: "usePer",
+          scopedSlots: { customRender: "usePer" }
+        },
+        {
+          title: "开通付费",
+          key: "openBuy",
+          dataIndex: "openBuy",
+          scopedSlots: { customRender: "openBuy" }
+        },
+        {
+          title: "购买次数包",
+          key: "buyNum",
+          dataIndex: "buyNum",
+          scopedSlots: { customRender: "buyNum" }
+        }
+      ],
+      qsdata: [
+        {
+          key: "1",
+          servName: "语音识别-普通话",
+          fwNum: "6,567千次",
+          syNum: "6,567千次",
+          tsyPer: "50%",
+          usePer: 45,
+          openBuy: "购买",
+          buyNum: "购买"
+        },
+        {
+          key: "2",
+          servName: "语音识别-英语",
+          fwNum: "567小时",
+          syNum: "6,567千次",
+          tsyPer: "30%",
+          usePer: 70,
+          openBuy: "购买",
+          buyNum: "购买"
+        },
+        {
+          key: "3",
+          servName: "语音识别-粤语",
+          fwNum: "567小时",
+          syNum: "6,567千次",
+          tsyPer: "24%",
+          usePer:50,
+          openBuy: "购买",
+          buyNum: "购买"
+        }
+      ],
+      ifShowDetail: false
     };
   },
   mounted() {
@@ -141,104 +234,126 @@ export default {
       }, 300);
     },
     initBarEcharts() {
-      var myChart = this.$echarts.init(document.getElementById("barID"));
-      var option = {
-        tooltip: {
-          trigger: "axis",
-          axisPointer: {
-            type: "cross",
-            crossStyle: {
-              color: "#999"
-            }
-          }
-        },
-        grid: {
-          left: "5%",
-          right: "7%",
-          bottom: "10%"
-        },
-        legend: {
-          data: ["蒸发量", "平均温度"]
-        },
-        xAxis: [
-          {
-            type: "category",
-            data: [
-              "1月",
-              "2月",
-              "3月",
-              "4月",
-              "5月",
-              "6月",
-              "7月",
-              "8月",
-              "9月",
-              "10月",
-              "11月",
-              "12月"
-            ],
+      setTimeout(() => {
+        var myChart = this.$echarts.init(document.getElementById("barID"));
+        var option = {
+          tooltip: {
+            trigger: "axis",
             axisPointer: {
-              type: "shadow"
-            }
-          }
-        ],
-        yAxis: [
-          {
-            type: "value",
-            name: "水量",
-            axisLabel: {
-              formatter: "{value} ml"
+              type: "cross",
+              crossStyle: {
+                color: "#999"
+              }
             }
           },
-          {
-            type: "value",
-            name: "温度",
-            axisLabel: {
-              formatter: "{value} °C"
-            }
-          }
-        ],
-        series: [
-          {
-            name: "蒸发量",
-            type: "bar",
-            data: [
-              2.0,
-              4.9,
-              7.0,
-              23.2,
-              25.6,
-              76.7,
-              135.6,
-              162.2,
-              32.6,
-              20.0,
-              6.4,
-              3.3
-            ]
+          grid: {
+            left: "5%",
+            right: "10%",
+            bottom: "10%"
           },
-          {
-            name: "平均温度",
-            type: "line",
-            yAxisIndex: 1,
-            data: [
-              2.0,
-              2.2,
-              3.3,
-              4.5,
-              6.3,
-              10.2,
-              20.3,
-              23.4,
-              23.0,
-              16.5,
-              12.0,
-              6.2
-            ]
-          }
-        ]
-      };
-      myChart.setOption(option);
+          legend: {
+            data: ["蒸发量", "平均温度"],
+            left: 10
+          },
+          xAxis: [
+            {
+              type: "category",
+              data: [
+                "1月",
+                "2月",
+                "3月",
+                "4月",
+                "5月",
+                "6月",
+                "7月",
+                "8月",
+                "9月",
+                "10月",
+                "11月",
+                "12月"
+              ],
+              axisPointer: {
+                type: "shadow"
+              }
+            }
+          ],
+          yAxis: [
+            {
+              type: "value",
+              name: "水量",
+              axisLabel: {
+                formatter: "{value} ml"
+              }
+            },
+            {
+              type: "value",
+              name: "温度",
+              axisLabel: {
+                formatter: "{value} °C"
+              }
+            }
+          ],
+          series: [
+            {
+              name: "蒸发量",
+              type: "bar",
+              data: [
+                2.0,
+                4.9,
+                7.0,
+                23.2,
+                25.6,
+                76.7,
+                135.6,
+                162.2,
+                32.6,
+                20.0,
+                6.4,
+                3.3
+              ],
+              itemStyle: {
+                normal: {
+                  color: "#5B8FF9"
+                }
+              }
+            },
+            {
+              name: "平均温度",
+              type: "line",
+              yAxisIndex: 1,
+              data: [
+                2.0,
+                2.2,
+                3.3,
+                4.5,
+                6.3,
+                10.2,
+                20.3,
+                23.4,
+                23.0,
+                16.5,
+                12.0,
+                6.2
+              ],
+              itemStyle: {
+                normal: {
+                  color: "#5B8FF9"
+                }
+              }
+            }
+          ]
+        };
+        myChart.setOption(option);
+      }, 200);
+    },
+    expanfangwen() {
+      this.ifShowDetail = !this.ifShowDetail;
+      var expanDom = document.getElementsByClassName("expan_btn_container")[0];
+      if (this.ifShowDetail) {
+        expanDom.style = "margin-top: 73px;";
+      } else {
+        expanDom.style = "margin-top: 15px;";
+      }
     }
   }
 };
@@ -274,7 +389,7 @@ export default {
   }
   .fangwen_area_container {
     width: 100%;
-    height: 316px;
+    min-height: 316px;
     margin-bottom: 13px;
     .title_container {
       width: 100%;
@@ -291,16 +406,34 @@ export default {
       width: 100%;
       height: 230px;
     }
+    .fangwen_detail_container {
+      width: 100%;
+      height: 200px;
+      margin-top: 15px;
+      /deep/ .ant-table-wrapper {
+        /deep/ .ant-spin-nested-loading {
+          /deep/ .ant-pagination {
+            display: flex;
+            justify-content: center;
+            float: inherit;
+          }
+        }
+      }
+    }
     .expan_btn_container {
       width: 100%;
       height: 26px;
       display: flex;
       align-items: center;
       justify-content: center;
+      margin-top: 15px;
       .btn_container1 {
         width: 19px;
         height: 19px;
         background-color: #6495fa;
+        &:hover {
+          cursor: pointer;
+        }
       }
     }
   }
@@ -323,9 +456,9 @@ export default {
     .table_container {
       width: 100%;
       flex: 1;
-      /deep/ .ant-table-wrapper{
-        /deep/ .ant-spin-nested-loading{
-          /deep/ .ant-pagination{
+      /deep/ .ant-table-wrapper {
+        /deep/ .ant-spin-nested-loading {
+          /deep/ .ant-pagination {
             display: flex;
             justify-content: center;
             float: inherit;
