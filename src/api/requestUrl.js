1ad7f04e4692644e698064165ecc326d6f4f0827
lixinjie <15110074982@139.com>
2021-01-20 10:54:17 +0800

    优化

--- src/api/requestUrl.js
+++ src/api/requestUrl.js
@@ -5,13 +5,9 @@ let urlData1 = '';
 
 if(process.env.NODE_ENV == 'development') { // 开发环境
     HTTPURL = window.config.url;
-    urlData = window.config.urlData;
-    urlData1 = window.config.urlData1;
 }else {
     HTTPURL = window.config.url;
-    urlData = window.config.urlData;
-    urlData1 = window.config.urlData1;
 }
 export {
-	HTTPURL,urlData,urlData1
+	HTTPURL
 }
