9074b545d9fc2eaa7fc0461c1dcf8f1b188d401d
lixinjie <15110074982@139.com>
2021-03-02 11:43:04 +0800

    优化

--- static/config/config.js
+++ static/config/config.js
@@ -1,5 +1,4 @@
-if (location.host == "www.yzsophia.com") {
-    window.config.url = "https://www.yzsophia.com"; //正式
-} else {
-     window.config.url = "http://dev-www.yzsophia.com"; //测式
-}
+window.config = {
+	url: "https://www.yzsophia.com",
+	testUrl: "http://dev-www.yzsophia.com"
+};
