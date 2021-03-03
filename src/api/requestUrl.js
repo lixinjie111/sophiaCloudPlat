297cfcf54f17f116811f7a0e17ea8adfd6bb35a5
zjj <660403@gdpr.com>
2021-03-03 16:30:32 +0800

    联调监控报表页面

--- src/api/requestUrl.js
+++ src/api/requestUrl.js
@@ -2,6 +2,7 @@
 let HTTPURL = '';
 // var devUrl = 'https://10.16.45.207:8101/admin';
 var devUrl = 'https://www.yzsophia.com/admin';
+var testDevUrl = 'https://test.yzsmart.top:38443/admin';
 
 if(process.env.NODE_ENV == 'development') { // 开发环境
     //HTTPURL = window.config.testUrl;
@@ -10,5 +11,5 @@ if(process.env.NODE_ENV == 'development') { // 开发环境
     HTTPURL = window.config.url;
 }
 export {
-	HTTPURL,devUrl
+	HTTPURL,devUrl,testDevUrl
 }
