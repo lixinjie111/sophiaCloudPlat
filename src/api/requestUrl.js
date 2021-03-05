2dfa00418f5a3d9c9d5f4b707d65dfc97bb026a9
lixinjie <15110074982@139.com>
2021-03-05 15:05:45 +0800

    优化

--- src/api/requestUrl.js
+++ src/api/requestUrl.js
@@ -1,17 +1,17 @@
 
-let HTTPURL = '';
+let HTTPURL =  window.config.url;
 // var devUrl = 'https://10.16.45.207:8101/admin';
-var devUrl = 'https://www.yzsophia.com/admin';
-var testDevUrl = 'https://test.yzsmart.top:38443/admin';
-console.log(process)
-if(process.env.NODE_ENV == 'development') { // 开发环境
-    HTTPURL = window.config.testUrl;
-    //HTTPURL = window.config.url;
-}else if(process.env.NODE_ENV == 'testing') { // 开发环境
-    HTTPURL = window.config.testUrl;
-}else{
-    HTTPURL = window.config.url;
-}
+// var devUrl = 'https://www.yzsophia.com/admin';
+// var testDevUrl = 'https://test.yzsmart.top:38443/admin';
+// console.log(process)
+// if(process.env.NODE_ENV == 'development') { // 开发环境
+//     HTTPURL = window.config.testUrl;
+//     //HTTPURL = window.config.url;
+// }else if(process.env.NODE_ENV == 'testing') { // 开发环境
+//     HTTPURL = window.config.testUrl;
+// }else{
+//     HTTPURL = window.config.url;
+// }
 export {
-	HTTPURL,devUrl,testDevUrl
+	HTTPURL//,devUrl,testDevUrl
 }
--- static/config/config.js
+++ static/config/config.js
@@ -1,4 +1,8 @@
 window.config = {
-	url: "https://www.yzsophia.com",
-	testUrl: "https://dev-www.yzsophia.com"
+	url: "",
 };
+if (location.host == "www.yzsophia.com") {
+    window.config.url = "https://www.yzsophia.com"; //正式
+} else {
+     window.config.url = "https://dev-www.yzsophia.com"; //测式
+}
