c1302e6dbdc0b3a7df64aa62cfaa4d0943825875
lixinjie <15110074982@139.com>
2021-03-05 14:23:56 +0800

    优化

--- src/api/requestUrl.js
+++ src/api/requestUrl.js
@@ -3,11 +3,13 @@ let HTTPURL = '';
 // var devUrl = 'https://10.16.45.207:8101/admin';
 var devUrl = 'https://www.yzsophia.com/admin';
 var testDevUrl = 'https://test.yzsmart.top:38443/admin';
-
+console.log(process)
 if(process.env.NODE_ENV == 'development') { // 开发环境
     HTTPURL = window.config.testUrl;
     //HTTPURL = window.config.url;
-}else {
+}else if(process.env.NODE_ENV == 'testing') { // 开发环境
+    HTTPURL = window.config.testUrl;
+}else{
     HTTPURL = window.config.url;
 }
 export {
