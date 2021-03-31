8c6c931b44ecb04039dd99228bcb313a834c1e13
fj <1076684287@qq.com>
2021-03-31 20:41:04 +0800

    111

--- src/components/application/list/ListPage.vue
+++ src/components/application/list/ListPage.vue
@@ -9,42 +9,68 @@
     </div>
     <init-page v-if="!data.length"></init-page>
     <template v-else>
-      <a-table :columns="columns" :data-source="data" @change="tableChange" bordered class="dotted-line-table" :row-selection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }">
-        <template slot="name" slot-scope="text">
-          <a class="c-blue name" @click="openModal(2)">{{ text }}</a>
+      <a-table :pagination="{ current, pageSize, total, showTotal:(total, range) => `第 ${range[0]}-${range[1]} / 共计 ${total} 条` }" :columns="columns" rowKey="id" :data-source="data" @change="tableChange" bordered class="dotted-line-table" :row-selection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }">
+        <template slot="appName" slot-scope="text, record">
+          <a class="c-blue name" @click="openModal(3, record)">{{ text }}</a>
         </template>
-        <template slot="secretKey" slot-scope="text, record">
-          <div>
-            <span v-if="showSecretKey.includes(record.key)">{{ text }}</span>
-            <span v-else>******</span>
-            <a-button type="link" @click="onShowSecretKey(record.key)">显示</a-button>
+        <div
+          slot="filterDropdown"
+          slot-scope="{ setSelectedKeys, selectedKeys, confirm, clearFilters, column }"
+          style="padding: 8px;"
+        >
+          <a-input
+            placeholder="搜索应用名称"
+            v-model="searchInputValue"
+            style="width: 188px; margin-bottom: 8px; display: block; border-radius: 5px"
+          >
+            <a-icon slot="suffix" type="search" />
+          </a-input>
+          <div style="border-top: 1px solid #e8e8e8; padding-top: 6px;">
+            <a-button
+              type="link"
+              size="small"
+              @click="onSearch"
+              style="width: 90px; margin-right: 8px"
+            >
+              搜索
+            </a-button>
+            <span style="width: 90px; display: inline-block; cursor: pointer; text-align: center" @click="() => searchInputValue = null">
+            重置
+          </span>
           </div>
+
+        </div>
+        <template slot="appSecret" slot-scope="text, record">
+          <div v-if="text">
+            <span v-if="showSecretKey.includes(record.id)">{{ text }}</span>
+            <span v-else>******</span>
+            <a-button type="link" @click="onShowSecretKey(record.id)">显示</a-button>
+          </div>
+          <template v-else>-</template>
+        </template>
+        <template slot="createTime" slot-scope="text, record">
+          <div v-if="text">
+            {{moment(text).format('YYYY-MM-DD HH:mm:ss')}}
+          </div>
+          <template v-else>-</template>
         </template>
         <span slot="action" slot-scope="text, record">
           <a class="action c-blue" @click="onReport(record.key)">报表</a>
-          <a class="action c-blue" @click="onDetail(record.key)">详情</a>
-          <a class="action c-blue" @click="onConfiguration(record.key)">配置</a>
+          <a class="action c-blue" @click="onDetail(record)">详情</a>
+          <a class="action c-blue" @click="onConfiguration(record)">配置</a>
           <a-popconfirm
              title="是否删除该应用?"
              ok-text="是"
              cancel-text="否"
-             @confirm="onDel(record.key)">
+             @confirm="onDel(record)">
             <a class="action action-del">删除</a>
           </a-popconfirm>
 
         </span>
-        <a-pagination
-          v-model="current"
-          :total="total"
-          show-size-changer
-          :page-size="pageSize"
-          :show-total="(total, range) => `第 ${range[0]}-${range[1]} / 共计 ${total} 条`"
-          @change="pageChange"
-        >
-        </a-pagination>
       </a-table>
-      <create-app-modal :visible="true" title="" v-if="showModal === 1" @cancel="closeModal" @next="onNext" @create="onCreate"/>
-      <connector-modal :visible="true" title=""  v-if="showModal === 2" @cancel="closeModal"/>
+      <create-app-modal :visible="true" :industryList="industryList" title="" v-if="showModal === 1" @cancel="closeModal" @refreshList="refreshList"/>
+      <connector-modal :visible="true" :interfaceList="interfaceList" :record="record" :interfaceData="interfaceData" title=""  v-if="showModal === 2" @cancel="closeModal"/>
+      <detail :visible="true" title="" :interfaceList="interfaceList" :industryList="industryList" :record="record" :interfaceData="interfaceData" v-if="showModal === 3" @cancel="closeModal"/>
     </template>
   </div>
 </template>
@@ -54,12 +80,14 @@
   import service from '@/utils/request'
   import CreateAppModal from './components/CreateAppModal.vue'
   import ConnectorModal from './components/ConnectorModal'
+  import Detail from './components/Detail'
   import InitPage from './InitPage.vue'
+  import { getApplicationList, getIndustryOptions, getServiceList, delApplication } from '@/api/application';
   const columns = [
     {
       title: '应用名称',
-      dataIndex: 'name',
-      scopedSlots: {customRender: 'name'},
+      dataIndex: 'appName',
+      scopedSlots: {customRender: 'appName', filterDropdown: 'filterDropdown' },
     },
     {
       title: 'APP ID',
@@ -72,13 +100,14 @@
     },
     {
       title: 'Secret Key',
-      dataIndex: 'secretKey',
-      scopedSlots: {customRender: 'secretKey'},
+      dataIndex: 'appSecret',
+      scopedSlots: {customRender: 'appSecret'},
     },
     {
       title: '创建时间',
       dataIndex: 'createTime',
       sorter: true,
+      scopedSlots: {customRender: 'createTime'},
     },
     {
       title: '操作',
@@ -87,32 +116,31 @@
       scopedSlots: {customRender: 'action'},
     },
   ];
-  const data = [];
-  for(let i = 0; i< 100; ++i){
-    data.push({
-      key: i,
-      name: '好生活',
-      appId: '2141',
-      appKey: 'appKey',
-      secretKey: '123456',
-      createTime: moment().format('YYYY-MM-DD HH:mm:ss'),
-    })
-  }
-
   export default {
-    components: { CreateAppModal, ConnectorModal, InitPage },
+    components: { CreateAppModal, ConnectorModal, InitPage, Detail },
     data() {
       return{
+        moment,
         columns,
         pageSize: 10,
         current: 1,
         total: 0,
         selectedRowKeys: [],
         sorter: {},
-        data,
+        data: [],
+        record: {},
         showModal: null,
         showSecretKey: [], // 显示secretKey的数据的id集合
+        interfaceList: [], // 服务接口列表
+        industryList: [], // 行业下啦
+        interfaceData: {}, // 配置信息
+        searchInputValue: null, // 搜索输入框的值
       }
+    },
+    mounted(){
+      this.getList()
+      this.getIndustryOptions()
+      this.getServiceList()
     },
     methods: {
       onShowSecretKey(key){
@@ -125,50 +153,71 @@
       // 报表列操作列点击事件
       onReport(key){},
       // 详情操作列点击事件
-      onDetail(key){
-        this.openModal(1)
+      onDetail(record){
+        this.openModal(3, record)
       },
       // 配置操作列点击事件
-      onConfiguration(key){
-        this.openModal(2)
+      onConfiguration(record){
+        this.openModal(2, record)
       },
       // 删除操作列点击事件
-      onDel(key){
-        console.log(this.$axios)
+      onDel(record){
+        delApplication({ id: record.id }).then(()=>{
+          this.$message.success('删除应用成功')
+          this.getList()
+        })
       },
-      pageChange(pagination, a){
-        console.log('pageChange', a)
+      pageChange(pagination){
         this.current = pagination.current;
         this.pageSize = pagination.pageSize;
         this.getList()
       },
       tableChange(pagination, filters, sorter){
-        this.sorter = sorter
-        console.log('tableChange', pagination, filters, sorter)
-        this.getList()
+        this.sorter = sorter;
+        this.pageChange(pagination)
       },
       // 行选择事件
       onSelectChange(a, b){
-        console.log(a, b)
         this.selectedRowKeys = a
       },
+      // 获取应用列表
       getList(){
-        const parmas = { current: this.current, pageSize: this.pageSize }
-        console.log('请求参数为===》', JSON.stringify(parmas))
+        const parmas = { pageIndex: this.current, pageSize: this.pageSize, appName: this.searchInputValue };
+        getApplicationList(parmas).then((res)=>{
+          this.data = res.data.list
+          console.log('res.data.total', res.data.total)
+          this.total = res.data.total
+        })
       },
-      openModal(showModal){
-        this.showModal = showModal
+      // 刷新应用列表
+      refreshList(){
+        this.current = 1;
+        this.getList()
+      },
+      openModal(showModal, record = {}){
+        this.record = record;
+        this.showModal = showModal;
       },
       closeModal(){
+        this.record = {}
         this.showModal = null
       },
-      onNext(values){
-        console.log('onNext', values)
-        this.showModal = 2
+      // 获取行业下拉选项
+      getIndustryOptions(){
+        getIndustryOptions().then((res)=>{
+          this.industryList = res.data
+        })
       },
-      onCreate(values){
-        console.log('onCreate', values)
-      }
+      // 接口配置项
+      getServiceList(){
+        getServiceList().then((res)=>{
+          this.interfaceList = res
+        })
+      }, // 搜索事件
+      onSearch(){
+        this.pageIndex = 1;
+        this.getList()
+      },
     }
   }
 </script>
