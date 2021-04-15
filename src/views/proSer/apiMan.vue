8e1d4b597f3961343545afca539f22e4068bff55
zjj <660403@gdpr.com>
2021-04-15 11:00:07 +0800

    bug修改

--- src/views/proSer/apiMan.vue
+++ src/views/proSer/apiMan.vue
@@ -17,7 +17,7 @@
           type="daterange"
           range-separator="至"
           start-placeholder="开始日期"
-          end-placeholder="结束日期"
+          end-placeholder="结束日期" 
           value-format="yyyy-MM-dd"
           @change="changeDataRange"
         ></el-date-picker>
