0dee6c130d5dc2ab84d76589d633830a08d0618b
zjj <660403@gdpr.com>
2021-04-23 09:35:43 +0800

    发票管理-发票详情页

--- src/views/invoiceMan/invoiceDetail.vue
+++ src/views/invoiceMan/invoiceDetail.vue
@@ -20,7 +20,47 @@
         ></vxe-grid>
       </div>
       <div class="label_txt">订单信息</div>
-      <div class="order_table"></div>
+      <div class="order_table">
+        <vxe-table
+          :align="allAlign"
+          :data="myTableData"
+          highlight-hover-row
+          show-header-overflow
+        >
+          <vxe-table-column
+            field="orderNum"
+            title="订单编号"
+          ></vxe-table-column>
+          <vxe-table-column field="proName" title="产品名称"></vxe-table-column>
+          <vxe-table-column
+            field="payTime"
+            title="订单支付时间"
+          ></vxe-table-column>
+          <vxe-table-column field="money" title="金额"></vxe-table-column>
+          <vxe-table-column
+            field="successTime"
+            title="成功时间"
+          ></vxe-table-column>
+        </vxe-table>
+        <vxe-pager
+          border
+          size="medium"
+          :loading="loading2"
+          :current-page="tablePage2.currentPage"
+          :page-size="tablePage2.pageSize"
+          :total="tablePage2.totalResult"
+          :layouts="[
+            'PrevPage',
+            'JumpNumber',
+            'NextPage',
+            'FullJump',
+            'Sizes',
+            'Total',
+          ]"
+          @page-change="handlePageChange2"
+        >
+        </vxe-pager>
+      </div>
     </div>
   </div>
 </template>
@@ -33,46 +73,98 @@ export default {
       fpNum: 1,
       fpMoney: "123.00",
       tableColumn: [],
-      tableData: []
+      tableData: [],
+      allAlign: null,
+      myTableData: [
+        {
+          orderNum: "Test1",
+          proName: "Develop",
+          payTime: "Man",
+          money: 28,
+          successTime: "vxe-table 从入门到放弃",
+        },
+        {
+          orderNum: "Test2",
+          proName: "Test",
+          payTime: "Women",
+          money: 22,
+          successTime: "Guangzhou",
+        },
+        {
+          orderNum: "Test3",
+          proName: "PM",
+          payTime: "Man",
+          money: 32,
+          successTime: "Shanghai",
+        },
+        {
+          orderNum: "Test4",
+          proName: "Designer",
+          payTime: "Women ",
+          money: 24,
+          successTime: "Shanghai",
+        },
+      ],
+      loading2: false,
+      tablePage2: {
+        currentPage: 1,
+        pageSize: 10,
+        totalResult: 0,
+      },
     };
   },
   created() {
     console.log(this.$route.query.detailData, "detailData");
     const myColumns = [
-              { field: 'name', title: '所属账号' },
-              { field: 'role', title: '发票性质' },
-              { field: 'sex', title: '发票类型' },
-              { field: 'age', title: '申请时间' },
-              { field: 'address', title: '退票原因' }
-            ]
+      { field: "name", title: "所属账号" },
+      { field: "role", title: "发票性质" },
+      { field: "sex", title: "发票类型" },
+      { field: "age", title: "申请时间" },
+      { field: "address", title: "退票原因" },
+    ];
     const myData = [
-        { id: 10001, name: 'Test1', nickname: 'T1', role: 'Develop', sex: 'Man', age: 28, address: 'Shenzhen' }
-    ]
-    this.reverseTable(myColumns, myData)
+      {
+        id: 10001,
+        name: "Test1",
+        nickname: "T1",
+        role: "Develop",
+        sex: "Man",
+        age: 28,
+        address: "Shenzhen",
+      },
+    ];
+    this.reverseTable(myColumns, myData);
   },
   methods: {
-    reverseTable (columns, list) {
-        const buildData = columns.map(column => {
-        const item = { col0: column.title }
+    handlePageChange2 ({ currentPage, pageSize }) {
+        this.tablePage2.currentPage = currentPage
+        this.tablePage2.pageSize = pageSize
+        // this.getfpManData()
+    },
+    reverseTable(columns, list) {
+      const buildData = columns.map((column) => {
+        const item = { col0: column.title };
         list.forEach((row, index) => {
-            item[`col${index + 1}`] = row[column.field]
-        })
-        return item
-        })
-        const buildColumns = [{
-        field: 'col0',
-        fixed: 'left',
-        width: 80
-        }]
-        list.forEach((item, index) => {
+          item[`col${index + 1}`] = row[column.field];
+        });
+        return item;
+      });
+      const buildColumns = [
+        {
+          field: "col0",
+          fixed: "left",
+          width: 80,
+        },
+      ];
+      list.forEach((item, index) => {
         buildColumns.push({
-            field: `col${index + 1}`,
-            minWidth: 120
-        })
-        })
-        this.tableData = buildData
-        this.tableColumn = buildColumns
-    }
+          field: `col${index + 1}`,
+          minWidth: 120,
+        });
+      });
+      this.tableData = buildData;
+      this.tableColumn = buildColumns;
+    },
   },
 };
 </script>
@@ -122,8 +214,11 @@ export default {
       color: #121c33;
       margin: 20px 0;
     }
-    .fp_table{
-        width: 100%;
+    .fp_table {
+      width: 100%;
+    }
+    .order_table {
+      width: 100%;
     }
   }
 }
