2abde906b344d1f5c59e1a354c36669e78d69823
zjj <660403@gdpr.com>
2021-04-27 15:54:06 +0800

    发票信息管理

--- src/views/invoiceMan/index.vue
+++ src/views/invoiceMan/index.vue
@@ -138,7 +138,7 @@
                 </vxe-pager>
             </el-tab-pane>
             <el-tab-pane label="发票信息管理" name="second">
-
+                <vInvoInfo></vInvoInfo>
             </el-tab-pane>
             <el-tab-pane label="寄送地址管理" name="third">寄送地址管理</el-tab-pane>
             <el-tab-pane label="电子邮箱" name="fourth">电子邮箱</el-tab-pane>
@@ -151,6 +151,7 @@
 import moment from "moment";
 import "moment/locale/zh-cn";
 import {fapSearch,queryInvoiceBase} from "../../api/invoiceMan/index";
+import vInvoInfo from "./invoiceInfoMan";
 export default {
   name: "invoiceMan",
   data() {
@@ -205,6 +206,9 @@ export default {
       moneyNum(){
         return this.$myUtilsFn.commafy(this.$myUtilsFn.toNumber(this.money), { digits:2 })
       }
+  },
+  components:{
+      vInvoInfo
   },
   methods: {
     watchDetailGetMoney(){
---
+++ src/views/invoiceMan/invoiceInfoMan.vue
@@ -0,0 +1,278 @@
+<template>
+  <div class="invoiceInfoMan_con">
+    <div class="invoice_info">您的发票信息未完善，请先完善</div>
+    <div class="invoice_con">
+      <el-form
+        :model="IssuingTypeRuleForm"
+        :rules="IssuingTypeRules"
+        label-width="140px"
+        class="demo-ruleForm"
+      >
+        <el-form-item label="开具类型：" prop="IssuingType">
+          <el-radio-group
+            v-model="IssuingTypeRuleForm.IssuingType"
+            @change="changeIssuType"
+          >
+            <el-radio label="personal">个人</el-radio>
+            <el-radio label="company">企业</el-radio>
+          </el-radio-group>
+        </el-form-item>
+      </el-form>
+      <div class="person" v-if="IssuType === 'personal'">
+        <el-form
+          :model="personalRuleForm"
+          ref="personalRuleForm"
+          label-width="140px"
+          class="personalRuleForm"
+        >
+          <el-form-item label="发票抬头：" prop="personalInvoiveTitle">
+            <el-input
+              v-model="personalRuleForm.personalInvoiveTitle"
+              placeholder="个人"
+              disabled
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="发票类型：" prop="personalInvoiceType">
+            <el-input
+              v-model="personalRuleForm.personalInvoiceType"
+              placeholder="增值税普通发票"
+              disabled
+            ></el-input>
+            <el-tooltip class="item" effect="dark" placement="top">
+              <div slot="content">
+                增值税普通发票开给小规模纳税人或者开票资料不齐全的购买人，购买人取得后不<br />可以进行项税额抵扣。若您还有疑问，建议联系贵司财务确认后在提交开票需求。
+              </div>
+              <a-icon type="question-circle" />
+            </el-tooltip>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="personSubmitForm('personalRuleForm')"
+              >保存发票信息</el-button
+            >
+          </el-form-item>
+        </el-form>
+      </div>
+      <div class="company" v-if="IssuType === 'company'">
+        <el-form
+          :model="ruleForm"
+          :rules="rules"
+          ref="ruleForm"
+          label-width="140px"
+          class="demo-ruleForm"
+        >
+          <el-form-item label="发票抬头：" prop="invoiveTitle">
+            <el-input
+              v-model="ruleForm.invoiveTitle"
+              placeholder="请输入发票抬头"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="发票类型：" prop="invoiceType">
+            <el-radio-group
+              v-model="ruleForm.invoiceType"
+              @change="changeInvType"
+            >
+              <el-radio label="zzputong">
+                增值税普通发票
+                <el-tooltip class="item" effect="dark" placement="top">
+                  <div slot="content">
+                    增值税普通发票开给小规模纳税人或者开票资料不齐全的购买人，购买人取得后不<br />可以进行项税额抵扣。若您还有疑问，建议联系贵司财务确认后在提交开票需求。
+                  </div>
+                  <a-icon type="question-circle" /> </el-tooltip
+              ></el-radio>
+              <el-radio label="zzzhuanyong">
+                增值税专用发票
+                <el-tooltip class="item" effect="dark" placement="top">
+                  <div slot="content">
+                    增值税专用发票开给一般纳税人，申请时需要提供公司名称、税号、地址电话、开<br />户行名称及账号，一般纳税人批复，购买人取得后可以进行进项税额抵扣。增值税<br />专用发票只能开具纸质发票。若您还有疑问，建议联系贵司财务确认后在提交开票<br />需求。
+                  </div>
+                  <a-icon type="question-circle" />
+                </el-tooltip>
+              </el-radio>
+              <el-radio label="zuzhizz">
+                组织（非企业）增值税普通发票
+                <el-tooltip class="item" effect="dark" placement="top">
+                  <div slot="content">
+                    其他组织（如政府、公共事业、非盈利机构等），无税号
+                  </div>
+                  <a-icon type="question-circle" />
+                </el-tooltip>
+              </el-radio>
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item label="纳税人识别号：" prop="taIdeNu">
+            <el-input
+              v-model="ruleForm.taIdeNu"
+              placeholder="请输入纳税人识别号"
+            ></el-input>
+            <p>
+              请与贵公司财务人员核实后，仔细填写准确的三证合一后的社会统一信用代码或税务登记证号，否则将影响后续发票的正常使用
+            </p>
+          </el-form-item>
+          <el-form-item label="开户银行名称：" prop="bankName">
+            <el-input
+              v-model="ruleForm.bankName"
+              placeholder="请输入开户银行名称"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="开户账号：">
+            <el-input
+              v-model="ruleForm.bankNumber"
+              placeholder="请输入开户账号"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="注册场所地址：">
+            <el-input
+              v-model="ruleForm.regAdderss"
+              placeholder="请输入地址"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="注册固定电话：">
+            <el-input
+              v-model="ruleForm.regFixtel"
+              placeholder="请输入电话"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('ruleForm')"
+              >保存发票信息</el-button
+            >
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "invoiceInfoMan",
+  data() {
+    var taIdeNuvalida = (rule, value, callback) => {
+      var ifValb = /^((?![^A-z]+$)(?!\D+$)[A-z\d]{15}|(?![^A-z]+$)(?!\D+$)[A-z\d]{18}|(?![^A-z]+$)(?!\D+$)[A-z\d]{20})$/gi.test(
+        value
+      );
+      if (value === "") {
+        callback(new Error("非个人发票纳税人识别号不能为空!"));
+      } else if (!ifValb) {
+        callback(new Error("纳税人识别号由字母和数字组成，长度为15、18或20"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      IssuingTypeRuleForm:{
+        IssuingType: "company",
+      },
+      IssuingTypeRules:{
+        IssuingType: [
+          { required: true, message: "请选择开具类型", trigger: "change" },
+        ],
+      },
+      personalRuleForm:{
+          personalInvoiveTitle:'个人',
+          personalInvoiceType:'增值税普通发票'
+      },
+      ruleForm: {
+        invoiveTitle: "",
+        invoiceType: "zzputong",
+        taIdeNu: "",
+        bankName: "",
+        bankNumber: "",
+        regAdderss: "",
+        regFixtel: "",
+      },
+      rules: {
+        invoiveTitle: [
+          { required: true, message: "请输入发票抬头", trigger: "blur" },
+        ],
+        invoiceType: [
+          { required: true, message: "请选择发票类型", trigger: "change" },
+        ],
+        taIdeNu: [
+          { required: true, trigger: "blur", validator: taIdeNuvalida },
+        ],
+      },
+      zzputongInfo:
+        "增值税普通发票开给小规模纳税人或者开票资料不齐全的购买人，购买人取得后不\n可以进行项税额抵扣。若您还有疑问，建议联系贵司财务确认后在提交开票需求。",
+      IssuType: "company",
+    };
+  },
+  created() {},
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    personSubmitForm(formName){
+        this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+    changeInvType(e) {
+      if (e === "zzputong") {
+      } else if (e === "zzzhuanyong") {
+      }
+    },
+    changeIssuType(e) {
+      this.IssuType = e;
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.invoiceInfoMan_con {
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  .invoice_info {
+    width: 100%;
+    height: 32px;
+    background-color: rgba(255, 251, 230, 0.6);
+    padding-left: 40px;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+    font-family: PingFangSC-Regular, PingFang SC;
+    font-weight: 400;
+    color: rgba(0, 0, 0, 0.65);
+    line-height: 24px;
+  }
+  .invoice_con {
+    width: 70%;
+    min-height: 470px;
+    margin-top: 23px;
+    /deep/ .el-form .el-form-item__content p {
+      font-size: 10px;
+      font-family: PingFangSC-Regular, PingFang SC;
+      font-weight: 400;
+      color: #121c33;
+    }
+    .person {
+        width: 35%;
+        /deep/ .personalRuleForm .el-form-item .el-form-item__content .el-input .el-input__inner{
+            border: none;
+            background-color: inherit;
+        }
+        /deep/ .personalRuleForm .el-form-item .el-form-item__content{
+            display: flex;
+            align-items: center;
+        }
+    }
+  }
+}
+</style>
