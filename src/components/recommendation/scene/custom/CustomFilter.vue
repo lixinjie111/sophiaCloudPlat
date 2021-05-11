6ca9c9200f1e60127c4695b5d40012cc2d67bec7
lyd272230 <lyd272230@163.com>
2021-05-11 18:28:52 +0800

    add

--- src/components/recommendation/scene/custom/CustomFilter.vue
+++ src/components/recommendation/scene/custom/CustomFilter.vue
@@ -5,18 +5,41 @@
       <p>过滤规则</p>
       <div class="line"></div>
       <ul>
-        <li>
+        <li v-for="(item,index) in list" :key="item.name">
           <div>
-            <span>过滤规则1:</span>
-            <span class="item_name">物品曝光01</span>
+            <span>过滤规则{{index+1}}:</span>
+            <span class="item_name">{{item.name}}</span>
           </div>
-          <span class="item_del">删除</span>
-        </li>
+          <a-popconfirm
+            title="是否删除该条目?"
+            ok-text="是"
+            cancel-text="否"
+            @confirm="del(item,index)">
+            <a-button class="item_del" type="link" style="color: red">删除</a-button>
+          </a-popconfirm>            
+        </li>          
       </ul>
-      <a-button class="add_btn" @click="add" :width="100">
+      <a-button class="add_btn" @click="add" :width="100" v-if="list.length<5">
         <a-icon type="plus" />
       </a-button>
     </div>
+    <a-modal destroyOnClose v-model="addModal" title="基于物品的协同过滤" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }" destroyOnClose>
+        <a-form-model :model="dataForm" :rules="rules">
+        <a-form-model-item label="策略名称" prop="name">
+            <a-input placeholder="请输入策略名称" v-model="dataForm.name"/>
+        </a-form-model-item>
+        <a-form-model-item label="时间跨度（天）" prop="date">
+            <a-input placeholder="请输入时间跨度（天）" v-model="dataForm.date"/>
+        </a-form-model-item>
+        <a-form-model-item label="推荐商品数" prop="num">
+            <a-input-number placeholder="请输入推荐商品数" width="200" v-model="dataForm.num" :min="0" :max="100"/>
+        </a-form-model-item>
+      </a-form-model>
+      <template slot="footer">
+          <a-button key="submit" @click="handleOk">确认</a-button>
+          <a-button key="back" @click="handleCancel">取消</a-button>
+      </template>
+    </a-modal>    
   </div>
 </template>
 <script>
@@ -24,9 +47,36 @@ export default {
   name: "CustomFilter",
   props: {},
   data() {
-    return {};
+    return {
+        list:[],
+        dataForm:{
+            name:"",
+            date:"",
+            num:""
+        },
+        rules:{
+            name:[{required:true,message:"请输入策略名称",trigger:"blur"}],
+            date:[{required:true,message:"请输入时间跨度（天）",trigger:"blur"}],
+            num:[{required:true,message:"请输入推荐商品数",trigger:"blur"}]
+        }
+    };
   },
-  methods: {},
+  methods: {
+      del(item){
+          console.log(item,index)
+          this.list.splice(index,1)
+      },
+      add(){
+          this.addModal = true
+      },
+      handleCancel(){
+
+      },
+      handleOk(){
+          this.addModal = false
+          this.list.push({name:this.dataForm.name})
+      }      
+  },
 };
 </script>
 <style scoped lang="scss">
@@ -57,7 +107,8 @@ export default {
       }
     }
     .add_btn {
-      width: 50%;
+        margin-top: 20px;        
+        width: 50%;
     }
   }
 }
--- src/components/recommendation/scene/custom/CustomRun.vue
+++ src/components/recommendation/scene/custom/CustomRun.vue
@@ -1,27 +1,54 @@
 <template>
   <div class="custom_run">
-    请添加排序策略，您最多能创建3个排序策略，系统将按您创建的顺序执行排序策略
-    <div class="method_list">
-      <p>排序策略</p>
-      <div class="line"></div>
-      <ul>
-        <li>
-          <div>
-            <span>排序策略1:</span>
-            <span class="item_name">逻辑回归01</span>
+      <div class="scene_box">
+        <p class="title">运营策略: 商品推荐</p>
+        <ul>
+            <li>
+                <a-checkbox>商品多样性</a-checkbox>
+                <span>一次推荐请求返回的结果里面，相邻的<a-input></a-input>个商品，作者不重复</span>
+            </li>
+            <li>
+                <a-checkbox>根据店铺进行打散</a-checkbox>
+                <span>一次推荐请求返回的结果里面，相邻的<a-input></a-input>个商品，<a-input></a-input>级频道不重复</span>                
+            </li>
+            <li>
+                <a-checkbox>根据商品类型进行混排</a-checkbox>
+                <span>一次推荐请求返回的结果里面：物品类型为    
+                    <a-select default-value="lucy">
+                    <a-select-option value="jack">
+                        Jack
+                    </a-select-option>
+                    <a-select-option value="lucy">
+                        Lucy
+                    </a-select-option>
+                    <a-select-option value="disabled">
+                        Disabled
+                    </a-select-option>
+                    <a-select-option value="Yiminghe">
+                        yiminghe
+                    </a-select-option>
+                    </a-select>的物品占比为
+                    <a-input></a-input></span>                
+            </li>
+        </ul>
+      </div>
+      <div class="scene_box">
+        <p class="title">必推行为:有必推商品</p>
+        <div class="best_goods">
+          <div class="best_goods_title">
+            <p>筛选必推物品：</p>
+            <a-button type="primary" @click="toWillPush">必推物品池</a-button>
           </div>
-          <span class="item_del">删除</span>
-        </li>
-      </ul>
-      <a-button class="add_btn" @click="add" :width="100">
-        <a-icon type="plus" />
-      </a-button>
-    </div>
+          <AddBestGoods :list="recommendParams" :propertiesList="mustPushPropertiesList"></AddBestGoods>
+        </div>
+      </div>
   </div>
 </template>
 <script>
+  import AddBestGoods from "../AddBestGoods";
 export default {
   name: "CustomSort",
+  components:{AddBestGoods},
   props: {},
   data() {
     return {};
@@ -31,34 +58,36 @@ export default {
 </script>
 <style scoped lang="scss">
 .custom_run {
-  .method_list {
-    padding: 0 24px;
-    > p {
-      margin: 12px 0;
-      font-weight: 600;
-    }
-    .line {
-      width: 100%;
-      height: 0;
-      border-top: 1px dashed #666;
-    }
-    ul {
-      > li {
-        display: flex;
-        margin: 16px 0;
-        .item_name {
-          font-weight: 600;
+    .scene_box {
+      margin-bottom: 20px;
+      display: flex;
+      justify-content: flex-start;
+      align-items: flex-start;
+
+      .title {
+        width: 100px;
+      }
+
+      /deep/ .ant-checkbox-group {
+        /deep/ .ant-checkbox-group-item {
+          margin-bottom: 10px;
+          display: block;
+
+          &:last-child {
+            margin-bottom: 0;
+          }
         }
-        .item_del {
-          margin-left: 40px;
-          color: #2373ff;
-          cursor: pointer;
+      }
+
+      .best_goods {
+        width: 575px;
+
+        .best_goods_title {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
         }
       }
     }
-    .add_btn {
-      width: 50%;
-    }
-  }
 }
 </style>
