e868f3d6137209fa9d4b8a9283742e0fa85eef63
lyd272230 <lyd272230@163.com>
2021-06-28 18:04:54 +0800

    open地址更改

--- src/views/home.vue
+++ src/views/home.vue
@@ -740,8 +740,8 @@ import { userInfo } from '@/api/user';
           });
         } else if(e.key=='dataSer0'){
           this.breadArr.splice(0, 0, "数据服务", "数据平台");
-          window.open(`http://10.16.44.247:8080?token=${localStorage.getItem('yk-token')}`,'_blank')
-          // window.open(`https://dev-www.yzsophia.com/dfsys?token=${localStorage.getItem('yk-token')}`,'_blank')
+          // window.open(`http://10.16.44.247:8080?token=${localStorage.getItem('yk-token')}`,'_blank')
+          window.open(`https://dev-www.yzsophia.com/dfsys?token=${localStorage.getItem('yk-token')}`,'_blank')
           // console.log(window.config.url)
         }else {
           // if(this.menuList){return}
